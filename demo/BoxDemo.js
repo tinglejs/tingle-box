a6ff8680140e8b656e33d098721c7dd2edb3afab
拂山 <jiasong.js@alibaba-inc.com>
2015-12-19 00:12:29 +0800

    update demo

--- demo/BoxDemo.js
+++ demo/BoxDemo.js
@@ -23,7 +23,7 @@ class Demo extends React.Component {
   render() {
     let t = this;
     return <div>
-      <HBox style={{height:'100px'}}>
+      <HBox style={{height:'90px'}} className="demo">
         <VBox className="tBCa" flex={2}>
           <Box className="tBCd" flex={1}>1</Box>
           <Box className="tBCe" flex={2}>2</Box>
@@ -32,19 +32,19 @@ class Demo extends React.Component {
         <Box className="tBCc" flex={1}>1</Box>
       </HBox>
 
-      <HBox style={{height:'100px'}} vAlign="start">
+      <HBox style={{height:'90px'}} vAlign="start" className="demo">
         <Box className="tBCa">a</Box>
         <Box className="tBCb">b</Box>
         <Box className="tBCc">c</Box>
       </HBox>
 
-      <HBox style={{height:'100px'}} vAlign="center">
+      <HBox style={{height:'90px'}} vAlign="center" className="demo">
         <Box className="tBCa">a</Box>
         <Box className="tBCb">b</Box>
         <Box className="tBCc">c</Box>
       </HBox>
 
-      <HBox style={{height:'100px'}} vAlign="end">
+      <HBox style={{height:'90px'}} vAlign="end" className="demo">
         <Box className="tBCa">a</Box>
         <Box className="tBCb">b</Box>
         <Box className="tBCc">c</Box>
